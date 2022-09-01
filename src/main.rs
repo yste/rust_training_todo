dfd7d025798910d20fb05dd717ba978b3e9cc982
takeru.morooka <takeru.morooka@play.jp>
2022-09-01 17:49:14 +0900

    error msg

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use structopt::StructOpt;
 use cli::{Action::*, CommandLineArgs};
 use tasks::Task;
 use std::path::PathBuf;
+use anyhow::anyhow;
 
 fn find_default_journal() -> Option<PathBuf> {
     home::home_dir().map(|mut path| {
@@ -13,7 +14,7 @@ fn find_default_journal() -> Option<PathBuf> {
     })
 }
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     // get cmd args
     let CommandLineArgs {
         action,
@@ -21,12 +22,14 @@ fn main() {
     } = CommandLineArgs::from_args();
 
     // unpack file
-    let journal_file = journal_file.or_else(find_default_journal).expect("Failed to find file");
+    let journal_file = journal_file.or_else(find_default_journal).ok_or(anyhow!("Failed to find journal"))?;
 
     // perform action
     match action {
         Add { text } => tasks::add_task(journal_file, Task::new(text)),
         List => tasks::list_tasks(journal_file),
         Done { position } => tasks::complete_task(journal_file, position),
-    }.expect("Failed to perform action")
+    }?;
+
+    Ok(())
 }
